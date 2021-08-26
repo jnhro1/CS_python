89dc22f6042c51b9a95d58c24dc41d43291bb881
jnhro1 <jnhro1@gmail.com>
2021-08-26 19:10:34 +0900

    20210826

--- 지헌/초성퀴즈.py
+++ 지헌/초성퀴즈.py
@@ -4,6 +4,12 @@
 3. 만약 힌트를 입력한다면 힌트주기
 4.포기라고 입력하면 "Game failed"가 뜨면서 종료
 '''
+def giveup():
+    print("아쉽게도 정답이 아닙니다.. 너무 어려우시면 '포기' or '힌트'라고 입력해주세요")
+def hint_message():
+    print("힌트를 보시러면 '힌트'라고 입력해주세요")
+heart = 3
+heart_icon = "♥"
 #시작
 print("초성게임을 시작하시겠습니까? 예 / 아니오")
 while True:
@@ -24,10 +30,13 @@ score = 0
 
 #문제1
 print("이번 주제는 햄버거입니다")
-print("힌트를 보시러면 '힌트'라고 입력해주세요")
+hint_message()
 print("ㅂㅁ")
 
 while True:
+    for i in range(1,heart+1):
+        print(heart_icon, end = " ")
+    print("")
     answer1 = input("정답을 입력해주세요 : ")
     if answer1 == "빅맥":
         score += 1
@@ -43,16 +52,25 @@ while True:
         print("총 점수는 %d점임 " %score)
         exit()
     else:
-        print("아쉽게도 정답이 아닙니다.. 너무 어려우시면 '포기' or '힌트'라고 입력해주세요")
+        giveup()
+        heart -= 1
+        if heart == 0:
+            print("흐어접")
+            print("총 점수는 %d입니다"%score)
+            exit()
         continue
+
         
     
 #두번째 문제
 print("두번째 문제 주제는 아이스크림입니다")
-print("힌트를 보시려면 '힌트'를 입력해주세요!")
+hint_message()
 print("ㅇㅅㄹㅌ")
 
 while True:
+    for i in range(1,heart+1):
+        print(heart_icon, end = " ")
+    print("")
     answer2 = input("정답을 입력해주세요 : ")
     if answer2 == "엑설런트" :
         score += 1
@@ -67,14 +85,22 @@ while True:
         print("포기하셨군요 잘가세요 ^^")
         exit()
     else:
-        print("아쉽게도 정답이 아닙니다.. 너무 어려우시면 '힌트' or '포기'라고 입력해주세요")
+        giveup()
+        heart -= 1
+        if heart == 0:
+            print("목숨 엔드!")
+            print("총 점수 %d입니다"%score)
+            exit()
         continue
 #3번째 문제
-print("이번 주제는  입니다 (난이도 극악)")
-print("힌트를 보시려면 '힌트'를 입력해주세요!")
+print("이번 주제는 게임 입니다")
+hint_message()
 print("ㅅㅅㅁㅍㅎ")
 
 while True:
+    for i in range(1,heart+1):
+        print(heart_icon, end = " ")
+    print("")
     answer3 = input("정답을 입력해주세요 : ")
     if answer3 == "산소미포함" :
         score += 1
@@ -89,13 +115,21 @@ while True:
         print("잘가셈 ㅂㅂ")       
         exit()
     else:
-        print("틀렸습니다 힌트를 보시거나 '포기'라고 입력해주세요")
+        giveup()
+        heart -= 1
+        if heart == 0:
+            print("허접은 아니군 하지만 탈락")
+            print("총 점수는 %d점 이였다"%score)
+            exit()
         continue
 #4번째문제
 print("이번 주제는 사자성입니다 ")
-print("힌트를 보시려면 '힌트'를 입력해주세요")
+hint_message()
 print("ㄷㅇㄱㅇ")
 while True:
+    for i in range(1,heart+1):
+        print(heart_icon, end = " ")
+    print("")
     answer4 = input("정답을 입력해주세요 : ")
     if answer4 == "도원결의" : 
         score += 1
@@ -110,15 +144,24 @@ while True:
         print("안녕히가세요")
         exit()
     else:
-        print("틀림")
+        giveup()
+        heart -= 1
+        if heart == 0:
+            print("목숨이 다되어 게임이 끝남")
+            print("최종 점수는 %d점임"%score)
+            exit()
+        
 #문제 파이브
 print("이번 주제는 애니메이션 입니다")
-print("힌트를 보시려면 '힌트'를 입력하거나 '포기'라고 입력해주세요")
+hint_message()
 print("ㄱㅁㅇㅋㄴ")
 
 while True:
+    for i in range(1,heart+1):
+        print(heart_icon, end = " ")
+    print("")
     answer5 = input("정답을 입력해주세요 : ")
-    if answer5 == "귀멸의 칼날" :
+    if answer5 == "귀멸의 칼날" or "귀멸의칼날":
         score += 1
         print("정답입니다!")
         break
@@ -127,10 +170,18 @@ while True:
         continue
     elif answer5 == "포기" :
         print("총 점수는 %d점임 " %score)
-        print("꺼져")
+        print("ㅂㅂ")
         exit()
     else:
-        print("틀렸습니다 힌트를 보시거나 포기라고 입력해주세요")
-        continue
-print("모든 문제를 맞춤 ㅊㅊ")
-print("최종 점수는 %d점입니다"%score)
+        giveup()
+        heart -= 1
+        if heart == 0:
+            print("마지막인데 목숨이 다되어 아쉽군요")
+            print("최종점수는 %d점 이었습니다" %score)
+            continue
+if heart == 3:
+    print("당신은 초성퀴즈의 고수입니다(히든엔딩)")
+
+print("모든 문제를 풀었습니다 축하합니다")
+print("최종 점수는 %d점입니다" %score)
+print("최종 목숨은 %d개였습니다" %heart)
